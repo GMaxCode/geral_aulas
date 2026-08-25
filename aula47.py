8d344b7c97e7fa04c068136437d40392c654fb48
gomes_dev <guilhermegomesmaximo@gmail.com>
2026-08-25 08:58:07 -0300

    Delete aula47.py

--- aula47.py
+++
@@ -1,8 +0,0 @@
-lista = [10 , 30 , 50, 20 , 20 , ]
-lista.append(55) # adciona o item na lista
-lista.pop(2) # deleta e devolve o item pra você
-lista.append(90)
-lista.append(200)
-del lista[3]# para deletar algum item da lista
-lista.insert(4,67) # adciona um item no indice escolhido
-print(lista)
