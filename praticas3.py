c4b0ed8e00489298b7ad351dc71dc73260e3a9de
gomes_dev <guilhermegomesmaximo@gmail.com>
2026-08-25 09:11:05 -0300

    Delete praticas3.py

--- praticas3.py
+++
@@ -1,9 +0,0 @@
-# maior de idade, se for maior de idade mostre algo...
-while True:
-    idade = int(input('digite sua idade: '))
-
-    if idade >= 18:
-        print('você está liberado para dirigir!!')
-
-    else:
-        print('você é de menor e não pode dirigir!!')
