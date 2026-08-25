f086037cf16092dda418b95d265d5836e543441b
gomes_dev <guilhermegomesmaximo@gmail.com>
2026-08-25 09:36:02 -0300

    Delete praticas8.py

--- praticas8.py
+++
@@ -1,7 +0,0 @@
-contagem = {
-    "nome": "Guilherme", 
-    "idade":20, 
-    "curso": "ADS"
-}
-for xica, valor in contagem.items():  #xica são as chaves, #valor são os valores
-    print(f'{xica}: {valor}')
