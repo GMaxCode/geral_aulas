6ba8e088824c542d212eeada8d9afcbbe49181f7
gomes_dev <guilhermegomesmaximo@gmail.com>
2026-08-27 13:36:08 +0000

    aula 52 de for loop

---
+++ python/python/aula52.py
@@ -0,0 +1,16 @@
+#trabalhando com loop for
+
+print('- BEM VINDO -')
+
+dic = {
+    'preto':4.60,
+    'branco':7,
+    'vermelho':8.90,
+    'amarelo':5
+}
+print(dic)
+
+for i in dic:
+    print(dic['vermelho'])
+    break
+  
